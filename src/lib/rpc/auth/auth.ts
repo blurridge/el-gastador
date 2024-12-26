5d9e6305cd832ef9d14edd1144cf96aacd4b1175
Zach Riane Machacon <zachriane01@gmail.com>
2024-12-26 19:46:04 +0800

    fix: removed unused cookie storage of user data

--- src/lib/rpc/auth/auth.ts
+++ src/lib/rpc/auth/auth.ts
@@ -1,6 +1,5 @@
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
-import { deleteCookie, setCookie } from "hono/cookie";
 import { createSupabaseServerClient } from "@/lib/supabase/server";
 import { createResponse } from "@/utils/createResponse";
 import { RESPONSE_STATUS } from "@/utils/constants";
@@ -13,13 +12,12 @@ const authRoutes = new Hono()
             const next = c.req.query("next")
             if (code) {
                 const supabase = await createSupabaseServerClient()
-                const { data, error } = await supabase.auth.exchangeCodeForSession(code)
+                const { error } = await supabase.auth.exchangeCodeForSession(code)
                 if (error) {
                     console.error("Error while signing in with Provider ", error);
                     throw new HTTPException(401, { message: error.message });
                 }
-                setCookie(c, "user_data", JSON.stringify(data.user))
-                return c.redirect("/home") 
+                return c.redirect("/home")
             }
             return c.redirect("/login")
         },
@@ -32,7 +30,6 @@ const authRoutes = new Hono()
                 console.error("Error while signing out with Provider ", error);
                 throw new HTTPException(401, { message: error.message });
             }
-            deleteCookie(c, "user_data")
             return c.json(createResponse({ status: RESPONSE_STATUS.SUCCESS, message: "User signed out successfully!" }));
         },
     )
